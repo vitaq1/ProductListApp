07367f5e231039e2b0909f3f53abc0c9389c8f7b
lancy <vshust505@gmail.com>
2022-03-08 16:49:03 +0300

    change api class

--- app/src/main/java/by/bsuir/vshu/productlistapp/data/remote/FakeStoreAPI.kt
+++ app/src/main/java/by/bsuir/vshu/productlistapp/data/remote/FakeStoreAPI.kt
@@ -1,11 +1,14 @@
 package by.bsuir.vshu.productlistapp.data.remote
 
+import by.bsuir.vshu.productlistapp.data.remote.dto.ItemDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface FakeStoreAPI {
 
     @GET("/products")
-    suspend fun getItems()
-    
+    suspend fun getItems(): List<ItemDto>
 
+    @GET("/products/{itemId}")
+    suspend fun getItemById(@Path("itemId") itemId: String): ItemDto
 }
